8b5b694bb02fe889c346a4ca006f0dd5cc657a4f
WailBouymaj-Kenzup <wail.bouymaj@kenzup.com>
2024-08-09 01:40:48 +0100

    add type check

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -11,12 +11,12 @@ async function Breed({ params }: { params: { slug: string } }) {
         <h1 className='text-2xl md:text-3xl'>{breed?.name}</h1>
         <Image className='object-cover rounded-xl w-[30dvh]' src={breedImage[0]?.url} width={breedImage[0]?.width} height={breedImage[0]?.height} alt={breed.name} />
         <div className='capitalize flex flex-col rounded-md bg-gray-900 p-4 max-w-3xl text-xs md:text-lg gap-2'>
-          <span>Weight: {breed.weight.metric}</span>
-          <span>temperament: {breed.temperament}</span>
-          <span>origin: {breed.origin}</span>
-          <span>life span: {breed.life_span}</span>
-          <span>description: {breed.description}</span>
-          <span>For more information <Link className='text-blue-500' href={breed.wikipedia_url}>click here</Link></span>
+          <span>Weight: {breed?.weight?.metric}</span>
+          <span>temperament: {breed?.temperament}</span>
+          <span>origin: {breed?.origin}</span>
+          <span>life span: {breed?.life_span}</span>
+          <span>description: {breed?.description}</span>
+          <span>For more information <Link className='text-blue-500' href={breed.wikipedia_url ?? ''}>click here</Link></span>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
           {breedImage.length > 1 && breedImage.slice(1).map((image, key) => {
